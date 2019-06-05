d29ba79dfa9ae3fdc811b1a81842d319bdec9626
ancanica <ancanica@IAS-9940000933>
2019-06-05 17:54:14 +0300

    Tema 7.1, ex. 1

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp19
+{
+    class ReadAndPrintFiles
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\ancanica\Desktop\TAM";
+            Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(File.ReadAllText(filePath));
+            }
+           
+            catch (PathTooLongException)
+            {
+                Console.WriteLine("The path exceed the maximum lenght.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("The file path is incorrect.");
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine("The format of the path is invalid.");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("The specified path is invalid.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file was not found.");
+            }
+          
+            catch (IOException)
+            {
+                Console.WriteLine("An I/O error occured.");
+            }
+            catch (SecurityException)
+            {
+                Console.WriteLine("You don't have permission to access this file.");
+            }
+        }
+    }
+}
